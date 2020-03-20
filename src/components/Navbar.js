df29deee3847c0e52c78cd6e38fc501bd19f8ed2
Raphael2790 <rssshev@gmail.com>
2020-03-20 05:14:06 -0300

    Created first component and iniciate routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        Hello from Navbar
+      </div>
+    )
+  }
+}
+
