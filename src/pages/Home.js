a1a1b37a0d2dae08e840e182107302c237c5f13b
Raphael2790 <rssshev@gmail.com>
2020-03-23 04:52:43 -0300

    Creating more components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero'
 
-export const Home = () => {
+export default function Home () {
   return (
-    <div>
-      Hello from Home Page
-    </div>
+    <Hero hero="defaultHero"/>
   )
 }
-
-export default Home
