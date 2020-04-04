7277279f2234b99cf229ed2cef8f3fd00434f7ba
Raphael2790 <rssshev@gmail.com>
2020-04-04 15:31:15 -0300

    Finished copy of the project freecodecamp beach resort

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -20,6 +20,12 @@ export default function RoomsFilter({rooms}) {
     return <option value={item} key={index}>{item}</option>
   })
 
+  let capacities = getUnique(rooms,'capacity');
+
+  capacities = capacities.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+  })
+
   return (
     <section className="filter-container">
       <Title title="search rooms"/>
@@ -30,6 +36,42 @@ export default function RoomsFilter({rooms}) {
           onChange={handleChange}>
             {types}
           </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select name="capacity" value={capacity} className="form-control"
+          onChange={handleChange}>
+            {capacities}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+            <input type="range" name="price" id="price" min={minPrice}
+            max={maxPrice} className="form-control" value={price} 
+            onChange={handleChange}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="size">room size</label>
+          <div className="size-inputs">
+            <input type="number" name="minSize" id="size" 
+            value={minSize} className="size-input" 
+            onChange={handleChange}/>
+            <input type="number" name="maxSize" id="size" 
+            value={maxSize} className="size-input" 
+            onChange={handleChange}/>
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="single-extra">
+            <input type="checkbox" name="breakfast" id="breakfast"
+            checked={breakfast} onChange={handleChange}/>
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input type="checkbox" name="pets" id="pets"
+            onChange={handleChange} checked={pets}/>
+            <label htmlFor="pets">pets</label>
+          </div>
         </div>
       </form>
     </section>
